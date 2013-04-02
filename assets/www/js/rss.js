4391f23ac6b370e3778f190af3772569624b61b8
Luis Cipriani <lfcipriani@gmail.com>
2013-04-02 10:38:25 -0300

    testes do loader de RSS e a implementação dele

--- assets/www/js/rss.js
+++ assets/www/js/rss.js
@@ -1,30 +1,22 @@
-
-//successCallback(feed, entries)
-//errorCallback(status, text) 408 timeout, 400 bad request
-
 var RSS = {
-    load: function(feedUrl, successCallback, errorCallback) {
+    load: function(feedUrl, successCallback, errorCallback, options) {
+        options = options || {}
 
         $.ajax({
             dataType: "json",
-            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=' + encodeURIComponent(feedUrl) + '&num=20&callback=?',
-            timeout: 10000,
+            url: options.url || 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=' + encodeURIComponent(feedUrl) + '&num=20&callback=?',
+            timeout: options.timeout || 10000,
             success: function (data, textStatus) {
                 if (data.responseStatus == "200") {
                     successCallback(data.responseData.feed, data.responseData.feed.entries);
                 } else {
-                    console.log("Ocorreu um erro na hora de recuperar o feed.");
-                    console.log(data.responseStatus + " - " + data.responseDetails)
+                    errorCallback(data.responseStatus, "Houve um problema com o feed de RSS");
                 }
             },
             error: function (jqxhr, textStatus, error) {
-                var err = textStatus + ', ' + error;
-                console.log("Request Failed: " + err);
-                $('<p>Request Failed: ' + err + '</p>').appendTo('#result');
+                errorCallback("500", "Houve um problema com o Serviço do Google Feed API");
             }
         }); 
 
     }
-
-
-}
+};
--- assets/www/test/tests.js
+++ assets/www/test/tests.js
@@ -1,15 +1,6 @@
 module("RSS");
 
 asyncTest( "carregamento com sucesso da URL de feed", function() {
-
-/*    $.mockjax({*/
-        //url: 'https://ajax.googleapis.com/ajax/services/feed/*',
-        ////proxy: "/mocks/feed.json",
-        //responseText: "bla",
-        //status: 200,
-        //responseTime: 200
-    //});
-
     expect(2);
     RSS.load("http://www.eventos.usp.br/?event-types=evento-cientifico&feed=rss",
         function(feed, entries) {
@@ -18,6 +9,36 @@ asyncTest( "carregamento com sucesso da URL de feed", function() {
             start();
         },
         function(status, text) {
+        }
+    );
+});
+
+asyncTest( "URL de feed errada", function() {
+    expect(2);
+    RSS.load("http://www.eventos.usp.br/?event-types=evento-errado&feed=rss",
+        function(feed, entries) {
+        },
+        function(status, text) {
+            equal(status, "400", "Serviço da USP retorna Bad Request");
+            equal(text, "Houve um problema com o feed de RSS", "Mensagem de erro é retornada");
+            start();
+        }
+    );
+});
+
+asyncTest( "Serviço de feed do Google está com problema", function() {
+    expect(2);
+    RSS.load("http://www.eventos.usp.br/?event-types=evento-errado&feed=rss",
+        function(feed, entries) {
+        },
+        function(status, text) {
+            equal(status, "500", "Serviço do Google retorna Internal Server Error");
+            equal(text, "Houve um problema com o Serviço do Google Feed API", "Mensagem de erro é retornada");
+            start();
+        },
+        { 
+            url: "https://ajax.googleapis.com/ajax/wrong/feed/load?v=1.0&q=" + encodeURIComponent("http://www.eventos.usp.br/?event-types=evento-cientifico&feed=rss") + "&num=20&callback=?",
+            timeout: 1000
         }
     );
 });
